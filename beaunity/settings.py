08586a13468823d164e9bed998c6558b409eff2a
dennie <denitsa.sureva@icloud.com>
2025-06-07 12:56:32 +0200

    landing page set up

--- beaunity/settings.py
+++ beaunity/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+    'accounts',
+    'common',
 ]
 
 TAILWIND_APP_NAME = 'theme'
--- beaunity/urls.py
+++ beaunity/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("", include("common.urls")),
     path("accounts/", include("accounts.urls")),
+
 ]
