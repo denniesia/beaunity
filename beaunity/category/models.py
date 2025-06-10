dd1ee40bbab0430a1d69a7a0e93eff3c4c57b7f0
dennie <denitsa.sureva@icloud.com>
2025-06-10 15:39:35 +0200

    fix: category overview page

--- beaunity/category/models.py
+++ beaunity/category/models.py
@@ -1,12 +1,21 @@
 from django.db import models
+from slugify import slugify
 from beaunity.common.mixins import LastUpdatedMixin, CreatedAtMixin, CreatedByMixin
 # Create your models here.
 class Category(LastUpdatedMixin, CreatedAtMixin, CreatedByMixin):
     title = models.CharField(max_length=30)
-    description = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(null=True, blank=True, unique=True, editable=False)
 
     class Meta:
         verbose_name_plural = 'Categories'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
--- beaunity/category/views.py
+++ beaunity/category/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Category
-
+from .forms import CategoryCreateForm
 # Create your views here.
 class CategoryOverviewView(ListView):
     template_name = 'category/category-overview.html'
@@ -10,3 +10,7 @@ class CategoryOverviewView(ListView):
         return Category.objects.all()
 
 
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryCreateForm
+    template_name = 'category/category-create.html'
