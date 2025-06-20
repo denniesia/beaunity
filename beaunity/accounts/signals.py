b741ab334106ac4fd35467fcef975abc7d666eed
dennie <denitsa.sureva@icloud.com>
2025-06-20 19:12:04 +0200

    feat: user group signal

--- beaunity/accounts/signals.py
+++ beaunity/accounts/signals.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
+from django.contrib.auth.models import Group
 from django.db.models.signals import post_save
 from beaunity.accounts.models import Profile
 
@@ -9,3 +10,9 @@ UserModel = get_user_model()
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=UserModel)
+def add_user_to_default_group(sender, instance,created, **kwargs):
+    if created:
+        user_group = Group.objects.get(name='User')
+        instance.groups.add(user_group)
