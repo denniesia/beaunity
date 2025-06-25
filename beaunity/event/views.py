a7b1557c5f1cf51d94813cbfc1ad31b02c524c61
dennie <denitsa.sureva@icloud.com>
2025-06-25 14:06:42 +0200

    fix: events overview listview + filterbar

--- beaunity/event/views.py
+++ beaunity/event/views.py
@@ -27,20 +27,27 @@ class EventsOverviewView(ListView):
         if sort_by == 'Popularity':
             queryset = queryset.annotate(popularity=Count('events')).order_by('-popularity')
         elif sort_by == 'Online':
-            queryset = queryset.filter(online=True)
+            queryset = queryset.filter(is_online=True)
         elif sort_by == 'Public_events':
             queryset = queryset.filter(is_public=True)
         elif sort_by == 'Hosts':
-            queryset = queryset.order_by('host__name')
+            queryset = queryset.order_by('created_by__username')
         if archived:
             queryset = queryset.filter(end_time__lt=now())
 
         return queryset.distinct()
 
     def get_context_data(self, **kwargs):
-        events = Event.objects.all()
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         context['cities'] = Event.objects.values_list('city', flat=True).distinct()
 
+        request = self.request
+        filter_mode = any([
+            request.GET.get('city'),
+            request.GET.get('category'),
+            request.GET.get('sort_by'),
+            request.GET.get('archived')
+        ])
+        context['filter_mode'] = filter_mode
         return context
