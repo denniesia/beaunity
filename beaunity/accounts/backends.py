2978dbb57f01089adf56b41eede1d1fbdaddc8a3
dennie <denitsa.sureva@icloud.com>
2025-07-03 12:28:48 +0200

    fix: email or usernam backend

--- beaunity/accounts/backends.py
+++ beaunity/accounts/backends.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+
 
 UserModel = get_user_model()
 
@@ -7,13 +9,12 @@ class EmailOrUsernameModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None:
             username = kwargs.get(UserModel.USERNAME_FIELD)
+
         try:
-            user = UserModel.objects.get(email=username)
+            user = UserModel.objects.get(Q(email__iexact=username) | Q(username=username))
         except UserModel.DoesNotExist:
-            try:
-                user = UserModel.objects.get(username=username)
-            except UserModel.DoesNotExist:
-                return None
+            return None
 
         if user.check_password(password) and self.user_can_authenticate(user):
             return user
+        return None
