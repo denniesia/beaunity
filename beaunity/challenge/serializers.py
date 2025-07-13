3cab3410e3cb6e797a1c815a1c33138da4954c64
dennie <denitsa.sureva@icloud.com>
2025-07-13 14:51:42 +0200

    fix: challenge serializer

--- beaunity/challenge/serializers.py
+++ beaunity/challenge/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Challenge
 from beaunity.category.models import Category
 import bleach
+from drf_spectacular.utils import extend_schema_field
+
 
 class ChallengeSerializer(serializers.ModelSerializer):
     created_by = serializers.StringRelatedField(read_only=True)
@@ -15,7 +17,7 @@ class ChallengeSerializer(serializers.ModelSerializer):
         read_only=True,
     )
     progress = serializers.ReadOnlyField()
-    duration_in_weeks = serializers.ReadOnlyField()
+    duration_in_weeks = serializers.SerializerMethodField()
     is_approved = serializers.ReadOnlyField()
 
     class Meta:
@@ -27,3 +29,7 @@ class ChallengeSerializer(serializers.ModelSerializer):
         if len(plain_text.strip()) < 100:
             raise serializers.ValidationError("Description must be at least 100 characters (excluding formatting).")
         return value
+
+    @extend_schema_field(int)
+    def get_duration_in_weeks(self, obj):
+        return obj.duration_in_weeks
