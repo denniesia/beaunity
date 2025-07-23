68550dd8e9ec28f4b66dd9ef34e51b62aa80374b
dennie <denitsa.sureva@icloud.com>
2025-07-23 11:35:34 +0200

    feat: docs rolle management

--- beaunity/common/urls.py
+++ beaunity/common/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('about/', views.about_view, name='about'),
     path('search/', views.SearchView.as_view(), name='search'),
     path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
-    path('api/', include('beaunity.common.api_urls'))
-
+    path('api/', include('beaunity.common.api_urls')),
 ]
 handler403 = views.custom_permission_denied_view
