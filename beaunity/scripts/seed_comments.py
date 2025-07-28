f7c0e43730849a04fc584fb6cd0184f0a419a410
dennie <denitsa.sureva@icloud.com>
2025-07-28 09:39:56 +0200

    feat: more comments

--- beaunity/scripts/seed_comments.py
+++ beaunity/scripts/seed_comments.py
@@ -55,7 +55,49 @@ comments_data = [
         "content_type": event_content_type,
         "object_id": 4,
         "content": "This book has been on my to do list for ageeees"
-    }
+    },
+    {
+        "created_by": creator2,
+        "content_type": challenge_content_type,
+        "object_id": 1,
+        "content": "I'be been feeling lazy lately, so this could be a good way to energise!!"
+    },
+    {
+        "created_by": creator4,
+        "content_type": challenge_content_type,
+        "object_id": 1,
+        "content": "I send the link so my bestfriend so we could do it together ;))) *excited*"
+    },
+    {
+        "created_by": creator5,
+        "content_type": challenge_content_type,
+        "object_id": 2,
+        "content": "I could join with some tips, I have been journaling since i was 8."
+    },
+    {
+        "created_by": creator3,
+        "content_type": challenge_content_type,
+        "object_id": 3,
+        "content": "This is so motivating"
+    },
+    {
+        "created_by": creator3,
+        "content_type": challenge_content_type,
+        "object_id": 4,
+        "content": "Ive read so many good reviews of doing retinol, but i have always been scared to try. "
+    },
+    {
+        "created_by": creator5,
+        "content_type": challenge_content_type,
+        "object_id": 4,
+        "content": "There is nothing scary, you just have to begin with the smallest concentration of retinol in your serum."
+    },
+    {
+        "created_by": creator6,
+        "content_type": challenge_content_type,
+        "object_id": 4,
+        "content": "I can definitely see myself doing this challenge."
+    },
 ]
 
 
@@ -63,4 +105,4 @@ comments_data = [
 for c in comments_data:
     comment = Comment.objects.create(**c)
 
-    print(f"Created comment from user {c["created_by"]}")
+    print(f"Created comment from user {c["created_by"]}.")
