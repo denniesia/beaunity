a4b631883dba254ee800a43131794dfd1a949d5d
dennie <denitsa.sureva@icloud.com>
2025-07-30 15:33:43 +0200

    Fix SECRET_KEY config

--- beaunity/settings.py
+++ beaunity/settings.py
@@ -29,7 +29,7 @@ import cloudinary.uploader
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("THE_SECRET_KEY", None)
+SECRET_KEY = config("SECRET_KEY", None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
