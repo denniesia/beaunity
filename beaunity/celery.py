0270bc0093dc200015882c7b3a971d35e4408ad3
dennie <denitsa.sureva@icloud.com>
2025-07-31 23:22:57 +0200

    fix: celery.py to handle ssl

--- beaunity/celery.py
+++ beaunity/celery.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from celery.schedules import crontab
+from kombu import Connection
+import ssl
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beaunity.settings')
 
@@ -24,6 +27,13 @@ app.conf.beat_schedule = {
     }
 }
 
+app.conf.update(
+    broker_url=config("CELERY_BROKER_URL"),
+    result_backend=config("CELERY_RESULT_BACKEND"),
+    broker_use_ssl={
+        'ssl_cert_reqs': ssl.CERT_NONE
+    },
+)
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {}'.format(self.request))
