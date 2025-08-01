3aabd9d1aef98a4da24252f1305bc7c030b9ed9e
dennie <denitsa.sureva@icloud.com>
2025-08-01 17:17:38 +0200

    relocation of template 404 403

--- beaunity/common/views.py
+++ beaunity/common/views.py
@@ -4,7 +4,7 @@ from beaunity.common.tasks import send_approval_email
 from django.shortcuts import get_object_or_404
 
 def custom_permission_denied_view(request, exception=None):
-    return render(request, "common/403.html", status=403)
+    return render(request, "403.html", status=403)
 
 
 @login_required
